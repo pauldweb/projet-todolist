5aa422218bea4898772279d2ab82ec7fe29b6276
BastienVanhove <bastienv.capital@gmail.com>
2022-01-31 05:27:50 -0500

    login rooting work

--- assets/script/index.js
+++ assets/script/index.js
@@ -2,43 +2,21 @@
 let userid = "root" ;
 let passid = "pass" ;
 
-
-
-function fm () {
+const login = () => {
 
     let user = document.getElementById("userid").value ;
     let pass = document.getElementById("passid").value ;
 
+    if ( user == userid && pass == passid ) window.location = "assets/pages/todo.html"
 
-    if ( user == userid && pass == passid ) { 
+    else if  ( user == "" && pass== "") alert("il faut ecrire quelque chose")
 
-    alert ( "ok" ) ;
+    else if  ( user != userid && pass == passid ) alert ("wrong username")
 
-    }
+    else if ( user == userid && pass != passid ) alert ( " password error")
 
-    else if  ( user == "" && pass== "")
-     {  
-
-    alert("il faut ecrire quelque chose") 
-
-
-    }
-
-    else if  ( user != userid && pass == passid ) { 
-
-    alert ("wrong username") 
-
-    }
-
-
-    else if ( user == userid && pass != passid )
-    { 
-    alert ( " password error") 
-    }
-
-}
-
- 
-  
+    else alert("unknow err")
+    
+}  
 
 
